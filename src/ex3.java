323e2ebc8fb3cc19fe15131c2b54a7bfb319ae9d
jiiieunn2 <93915072+jiieunn2@users.noreply.github.com>
2023-11-20 15:54:05 +0900

    [Init] ex3.java

--- src/ex3.java
+++ src/ex3.java
@@ -1,18 +1,22 @@
-public class ex3 {
+public class Ex3 {
     public static void main(String[] args) {
         // 1. 다음과 같이 정사각형을 출력하는 코드 작성
         /*
         ----------
-                *****
+         *****
          *****
          *****
          *****
          *****
         ----------
                 */
-       
 
-        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         // 2. 다음과 같이 삼각형을 출력하는 코드 작성
         /*
@@ -24,12 +28,13 @@ public class ex3 {
          *****
         ----------
          */
-         for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 6; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
 
         // 3. 다음과 같이 삼각형을 출력하는 코드 작성
         /*
@@ -42,11 +47,17 @@ public class ex3 {
          ----------
          */
 
-        
-
+        for(Integer i=1; i < 10; i=i+2) {
+            for(Integer j=10; j > i; j=j-2) {
+                System.out.print(" ");
             }
-
+            for(Integer j=0; j < i; j=j+1) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
     }
+}
+
+    
 
